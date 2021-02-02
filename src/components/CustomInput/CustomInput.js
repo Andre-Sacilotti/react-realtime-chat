42a1682bc3dde1177c4f2b07a7b4ebf40fafd888
André Sacilotti <andre.sacilotti@estudante.ufscar.br>
2021-02-02 00:14:33 -0300

    Finalização mobile first da tela de login

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CustomInput = () => {
+import {FaUserAlt} from "react-icons/fa"
+
+const Input = styled.input`
+  border: 1px solid #A0A0A0;
+  height: 28px;
+  border-radius: 3px;
+  border-bottom-left-radius: 0px;
+  border-top-left-radius: 0px;
+  padding-left: 4px;
+  padding-right: 4px;
+  
+  
+`
+
+
+const InputDiv = styled.div`
+  display: inline-flex;
+  height: 100%;
+
+  padding-top: 10px;
+  padding-bottom: 10px;
+  
+`
+
+const InputIconDiv = styled.div`
+
+  background-color: #FFFFFF;
+  
+  border-bottom-left-radius: 3px;
+  border-top-left-radius: 3px;
+  border: 1px solid #A0A0A0;
+  border-right: 0px;
+  
+  justify-content: center;
+  text-align: center;
+  vertical-align: middle;
+  
+  padding-left: 5px;
+  padding-right: 5px;
+`
+
+const CustomInput = (props) => {
 
     return (
         <div>
-            a
+            <InputDiv>
+                <InputIconDiv>
+                    {props.icon}
+                </InputIconDiv>
+
+                <Input placeholder={props.children}/>
+            </InputDiv>
+
         </div>
     )
 }
--- src/components/LoginBox/LoginBox.js
+++ src/components/LoginBox/LoginBox.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from "styled-components"
 
 import CustomButton from "../CustomButton/CustomButton";
+import CustomInput from "../CustomInput/CustomInput";
+import {FaUserAlt} from "react-icons/fa";
+import {RiLockPasswordFill} from "react-icons/ri"
 
 const LoginBoxDiv = styled.div`
   border: 1px solid black;
@@ -11,9 +14,40 @@ const LoginBoxDiv = styled.div`
   background-color: #C4C4C4;
 `
 
+const ForgetPassDiv = styled.div`
+  font-weight: 100;
+  font-size: 12px;
+  margin-top: -8px;
+`
+
+
 const AnonymBtnDiv = styled.div`
   padding-top: 32px;
   padding-bottom: 16px
+`
+
+const LoginBtnDiv = styled.div`
+  padding-top: 16px;
+  padding-bottom: 16px
+`
+
+const FormsDiv = styled.div`
+  padding-top: 32px;
+  padding-bottom: 16px
+`
+
+const Logina = styled.a`
+  text-decoration: none;
+
+  &:active, &:visited {
+    text-decoration: none;
+    color: #323232;
+  }
+
+  &:hover {
+    color: black;
+  }
+
 `
 
 const LoginBox = () => {
@@ -23,7 +57,28 @@ const LoginBox = () => {
                 <AnonymBtnDiv>
                     <CustomButton dark={false}>Enter as Anonymus User</CustomButton>
                 </AnonymBtnDiv>
-                aaa
+
+                <FormsDiv>
+                    <CustomInput
+                    icon={<FaUserAlt style={{verticalAlign: 'bottom'}} />}
+                    >Username</CustomInput>
+
+                    <CustomInput
+                        icon={<RiLockPasswordFill style={{verticalAlign: 'bottom'}} />}
+                    >Password</CustomInput>
+
+                    <Logina href={""}>
+                        <ForgetPassDiv>Forgot your password?</ForgetPassDiv>
+                    </Logina>
+                </FormsDiv>
+
+                <LoginBtnDiv>
+
+                    <CustomButton dark={false}>Login</CustomButton>
+                </LoginBtnDiv>
+                <Logina href={""}>
+                    <ForgetPassDiv>Create new account.</ForgetPassDiv>
+                </Logina>
             </LoginBoxDiv>
     )
 }
