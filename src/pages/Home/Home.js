ed94ba213b1f26c23ffcf3202f8017aca685d1a0
André Sacilotti <andre.sacilotti@estudante.ufscar.br>
2021-02-07 01:08:35 -0300

    Esqueleto do chat mobile first

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
 import {Link} from "react-router-dom";
+import {TONE90} from "../../components/Colors";
 
 const HomeDiv = styled.div`
   width: 100%;
   height: 100%;
-  border: 1px solid pink;
   color: white;
+
 `
 
 const ChatDiv = styled.div`
-
+  background-color: ${TONE90};
   width: 100vw;
-  height: 100vh;
-  border: 1px solid pink;
+  height: 90vh;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+`
+
+const TitleDiv = styled.div`
+  height: 10vh;
+`
+
+const MessagesDiv = styled.div`
+  padding-top: 50px;
+ height: calc(85% - 50px);
+  overflow-y: auto;
+`
+
+const BottomBarDiv = styled.div`
+  height: 15%;
 `
 
 const Home = (props) => {
 
     return (
         <HomeDiv>
+            <TitleDiv>
+                Chat 1
+            </TitleDiv>
+
             <ChatDiv>
-                <Link to={"/teste"}> Mudar </Link>
-                <h2>Teste</h2>
+                <MessagesDiv>
+                    Messages
+                </MessagesDiv>
+                <BottomBarDiv>
+                    Send
+                </BottomBarDiv>
             </ChatDiv>
 
         </HomeDiv>
